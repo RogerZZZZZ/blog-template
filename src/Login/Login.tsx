654c37435b9efc10e7bbbbcadda14dd95ffb1817
RogerZZZZZ <501841352@qq.com>
2018-11-29 00:11:22 +0800

    let screen full size

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -8,19 +8,19 @@ import {
   Input, Card,
 } from 'antd'
 
-const { Header, Content } = Layout
+const { Header, Content} = Layout
 
 const LoginBox = ({classes}: IProps) => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
   return (
-    <Layout>
+    <Layout className={classes.fullHeight}>
       <Header>Header</Header>
       <Content>
         <Card className={classes.card}>
-          <Input placeholder='Username' value={username} onChange={e => setUsername(e.target.value)}/>
-          <Input placeholder='Password' value={password} onChange={e => setPassword(e.target.value)}/>
+          <Input placeholder='Username' value={username} onChange={e => setUsername(e.target.value)} className={classes.inputItem}/>
+          <Input placeholder='Password' value={password} onChange={e => setPassword(e.target.value)} className={classes.inputItem}/>
         </Card>
       </Content>
     </Layout>
@@ -30,6 +30,12 @@ const LoginBox = ({classes}: IProps) => {
 const Login = injectSheet({
   card: {
     width: '500px'
+  },
+  fullHeight: {
+    height: '100vh'
+  },
+  inputItem: {
+    marginBottom: '30px'
   }
 })(LoginBox)
 
